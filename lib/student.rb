530bb3417e10e6baeedcb2c51d64715e6d6d6747
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-02 03:04:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,13 @@ class Student
   end
   
   def self.create_table
-    sql = <<- 
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
   end
 
 
